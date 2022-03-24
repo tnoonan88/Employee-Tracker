76dac69ea1d96120e76fe1868e9f37a13aec50a9
tnoonan <tnoonan88@gmail.com>
2022-03-24 12:37:05 -0400

    debugged seeding

--- db/schema.sql
+++ db/schema.sql
@@ -21,9 +21,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY (role)
+    FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL
 );
