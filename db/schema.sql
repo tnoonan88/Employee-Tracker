a1ddfc5731864b4d3bb9ba8297a53c4267911911
tnoonan <tnoonan88@gmail.com>
2022-03-25 13:00:20 -0400

    removed manager hierarchy for better functionality

--- db/schema.sql
+++ db/schema.sql
@@ -23,6 +23,8 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,7 @@ INSERT INTO department (name)
 VALUES ("Sales"),
        ("Engineering"),
        ("Accounting"),
-       ("Legal"),
-       ("Management");
+       ("Legal");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Salesperson", 8320.00, 1), 
@@ -14,18 +13,16 @@ VALUES ("Salesperson", 8320.00, 1),
        ("Chief Financial Officer", 250000.00, 3),
        ("Accountant", 95000.00, 3),
        ("Head of Legal", 250000.00, 4),
-       ("Attorney", 175000.00, 4),
-       ("CEO", 350000.00, 5); 
+       ("Attorney", 175000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 2, 10),
-       ("Jim", "Bo", 1, 2),
-       ("Tom", "Noonan", 3, 10),
+VALUES ("John", "Doe", 2, NULL),
+       ("Jim", "Bo", 1, 1),
+       ("Tom", "Noonan", 3, NULL),
        ("Kait", "Witnach", 4, 3),
        ("David", "Blane", 5, 3),
-       ("Stevie", "Nicks", 6, 10),
+       ("Stevie", "Nicks", 6, NULL),
        ("Montell", "Williams", 7, 6),
-       ("Bilbo", "Baggins", 8, 10),
-       ("Dante", "Lingus", 9, 8),
-       ("Henry", "Bop", 10, null);
+       ("Bilbo", "Baggins", 8, NULL),
+       ("Dante", "Lingus", 9, 8);
 
