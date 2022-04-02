a2bce38c752cdc229ed43b4f9d5cb8f8c68ce35a
tnoonan <tnoonan88@gmail.com>
2022-04-02 19:26:42 -0400

    debugged update employee func

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ console.log('You are now connected to the Employee Database.')
 );
 
 db.connect((err) => {
-    if (err) 
-    throw err;
+    if (err) throw err;
     startApp();
 });
 
@@ -57,7 +56,7 @@ function startApp() {
         else {
             updateEmpRole()
         }
-        console.log(response);
+        // console.log(response);
     });
 };
 
@@ -148,4 +147,110 @@ function addRole() {
                             startApp();
                         })
                     })
-                })};
+})};
+
+function addEmp() {
+    db.query(
+        'SELECT * FROM role',
+        function(err, results) {
+            const role = results.map(role => {
+                return {name: role.title, value: role.id}
+                });
+                if (err) throw err;
+    db.query(
+        'SELECT * FROM employee WHERE manager_id IS NULL',
+        function(err, results) {
+            const manager = results.map(manager => {
+                return {name: manager.first_name+' '+manager.last_name, value: manager.id}
+                });
+                if (err) throw err;
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'first_name',
+                        message: 'New employees first name:'
+                    },
+                    {
+                        type: 'input',
+                        name: 'last_name',
+                        message: 'New employees last name:'
+                    },
+                    {
+                        type: 'list',
+                        name: 'role_id',
+                        message: 'Assign a role:',
+                        choices: role
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager_id',
+                        message: 'Assign a manager:',
+                        choices: manager
+                    }
+                ])
+                .then((response) => {
+                    db.query(
+                        `INSERT INTO employee SET ?`,
+                        response,
+                        function(err, results) {
+                            console.table('New employee added.');
+                            if (err) throw err;
+                            startApp();
+                        })
+                    })
+})})};
+
+function updateEmpRole() {
+    db.query(
+        'SELECT * FROM role',
+        function(err, results) {
+            const role = results.map(role => {
+                return {name: role.title, value: role.id}
+                });
+                if (err) throw err;
+    db.query(
+        'SELECT * FROM employee',
+        function(err, results) {
+            const employee = results.map(employee => {
+                return {name: employee.first_name+' '+employee.last_name, value: employee.id}
+                });
+                if (err) throw err;
+    db.query(
+        'SELECT * FROM employee WHERE manager_id IS NULL',
+        function(err, results) {
+            const manager = results.map(manager => {
+                return {name: manager.first_name+' '+manager.last_name, value: manager.id}
+                });
+                if (err) throw err;
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'id',
+                        message: 'Select an employee to update:',
+                        choices: employee
+                    },
+                    {
+                        type: 'list',
+                        name: 'role_id',
+                        message: 'Please assign a new role:',
+                        choices: role
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager_id',
+                        message: 'Please assign a manager:',
+                        choices: manager
+                    }
+                ])
+                .then((response) => {
+                    console.log(response)
+                    db.query(
+                        `UPDATE employee SET role_id = ?, manager_id = ? WHERE id = ?`,
+                        [response.role_id, response.manager_id, response.id],
+                        function(err, results) {
+                            console.table('Role successfully updated.');
+                            if (err) throw err;
+                            startApp()
+                        })
+                    })
+})})})};
